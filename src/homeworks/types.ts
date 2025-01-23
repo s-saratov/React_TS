293caf6e3b0453ac1d03f4713c46215d777c1e73
s-saratov <saratovs@yandex.ru>
2025-01-23 18:52:02 +0100

    homework_06 add

---
+++ src/homeworks/types.ts
@@ -0,0 +1,29 @@
+/*
+Задание
+
+Протипизировать следующие данные в соответствии с изученными правилами.
+*/
+
+let age: number = 25;  
+
+let name: string = "Alice";
+
+let isActive: boolean = true; 
+
+let nullableValue: null = null; // Строка или null
+
+let numbers: number[] = [1, 2, 3, 4]; 
+
+let names: string[] = ["Alice", "Bob", "Charlie"];
+
+let mixed: [number, string, number, string] = [1, "Alice", 2, "Bob"];
+
+let colors: [number, string, boolean, string] = [123, "red", true, "blue"];
+
+const add = (a: number, b: number): number => {
+  return a - b;
+};
+
+const logMessage = (message: string): void => {
+  console.log(message);
+};
