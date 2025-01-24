4a78f35d924d8d3138da90b859e8a218faf8c53d
s-saratov <saratovs@yandex.ru>
2025-01-24 09:27:18 +0100

    homework_06 improve

--- src/homeworks/types.ts
+++ src/homeworks/types.ts
@@ -10,15 +10,15 @@ let name: string = "Alice";
 
 let isActive: boolean = true; 
 
-let nullableValue: null = null; // Строка или null
+let nullableValue: string | null = null; // Строка или null
 
 let numbers: number[] = [1, 2, 3, 4]; 
 
 let names: string[] = ["Alice", "Bob", "Charlie"];
 
-let mixed: [number, string, number, string] = [1, "Alice", 2, "Bob"];
+let mixed: (number | string)[] = [1, "Alice", 2, "Bob"];
 
-let colors: [number, string, boolean, string] = [123, "red", true, "blue"];
+let colors: (number | string | boolean)[] = [123, "red", true, "blue"];
 
 const add = (a: number, b: number): number => {
   return a - b;
