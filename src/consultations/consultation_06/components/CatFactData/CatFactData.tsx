8d438a82c0718db8979c1b11d8c32c3511f6762e
s-saratov <saratovs@yandex.ru>
2025-02-07 23:18:36 +0100

    add consultation_07

--- src/consultations/consultation_06/components/CatFactData/CatFactData.tsx
+++ src/consultations/consultation_06/components/CatFactData/CatFactData.tsx
@@ -9,21 +9,42 @@ import {
 import { useContext } from "react";
 import { CatFactRandomizerContext } from "../CatFactRandomizer/CatFactRandomizer";
 
+// Деструктуризируем объект
+
 function CatFactData() {
   // Шаг 5.
-  const сatFact = useContext(CatFactRandomizerContext);
-  return (
-    <CatFactDataContainer>
-      <FactItem>
+  const catFact = useContext(CatFactRandomizerContext);
+  console.log(catFact);
+
+  const { data, setCatFactData } = catFact;
+
+const onDelete = (id: any) => {
+  setCatFactData((prevValue: any[]) => {
+    return prevValue.filter((factObj: any) => factObj.id !== id)
+  });
+  };
+
+  const catFacts = data.map((value) => {
+    return (
+      <FactItem key={value.id}>
         <FactItemControl>
-          <Fact>Hello cat!</Fact>
+          <Fact>{value.fact}</Fact>
           <ButtonControl>
-            <Button name="DELETE" isRed />
+            <Button name="DELETE" isRed onClick={() => onDelete(value.id)} />
           </ButtonControl>
         </FactItemControl>
       </FactItem>
-    </CatFactDataContainer>
-  );
+    );
+
+  });
+
+  return <CatFactDataContainer>{catFacts}</CatFactDataContainer>;
 }
 
 export default CatFactData;
+
+/* Дополнительные задания:
+  - создать кнопку "Удалить все факты";
+  - создать приложение "ToDoList" - в поле ввода указывается задача, которая при нажатии на кнопку попадает в общий список;
+  - у каждой задачи есть отдельная кнопка удаления
+*/
